5bbd0588884c5345f6a57ed9f7c650ceebff216b
Leon Vincii <leon.vincii@gmail.com>
2018-05-01 12:36:28 +1000

    ++ add sign in/sign up entries.

--- tdgas/admin.py
+++ tdgas/admin.py
@@ -5,7 +5,6 @@ from .models import *
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-
     fieldsets = (
         (None             , {'fields': ('email',
                                         'password',
@@ -28,10 +27,10 @@ class UserAdmin(UserAdmin):
     )
     add_fieldsets = (
         (None             , {'classes': ('wide',),
-                             'fields': ('email', 'password1', 'password2')}),
+                             'fields' : ('email', 'password1', 'password2')}),
     )
     list_display  = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
+    ordering      = ('email',)
 
 # Register your models below.
