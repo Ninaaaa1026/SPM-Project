31e2a904640d385f9fb4064cceaa2b10194e9e8c
Leon Vincii <leon.vincii@gmail.com>
2018-05-01 22:15:21 +1000

    ++ add sign up page.

--- tdgas/views.py
+++ tdgas/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html', {})
+def home_view(request):
+    return render(request, 'home.html', {'firstname': request.user.first_name})
+
+def signup_view(request):
+    if request.method == 'GET':
+        return render(request, 'registration/register.html', {})
