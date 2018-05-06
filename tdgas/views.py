df183ca7c7031fa58aec19109ef0a18d971dc574
jinnanl <jinnanl@student.unimelb.edu.au>
2018-05-06 15:53:42 +1000

    add available appointment time

--- tdgas/views.py
+++ tdgas/views.py
@@ -1,4 +1,4 @@
-from datetime                       import datetime
+from datetime import datetime, timedelta, date, time
 
 from django.contrib.auth            import login, authenticate
 from django.contrib.auth.decorators import login_required
@@ -179,7 +179,78 @@ def appointment_new_view(request):
         user = User.objects.get(email__exact = request.user.email)
         dogs = Dog.objects.filter(owner=user)
         firstname = request.user.first_name
-        return render(request, 'appointment_add.html', {'user': user, 'dogs': dogs, 'firstname': firstname})
+
+        date_from = date.today()
+        if date.today().weekday() == [0 - 3]:
+            if datetime.now().hour < 8 & datetime.now().minute < 30:
+                datetime_from_hour = 8
+                datetime_from_minute = 30
+            elif datetime.now().hour < 10:
+                datetime_from_hour = 10
+                datetime_from_minute = 0
+            elif datetime.now().hour < 12 & datetime.now().minute < 30:
+                datetime_from_hour = 12
+                datetime_from_minute = 30
+            elif datetime.now().hour < 14:
+                datetime_from_hour = 14
+                datetime_from_minute = 0
+            elif datetime.now().hour < 15 & datetime.now().minute < 30:
+                datetime_from_hour = 15
+                datetime_from_minute = 30
+            else:
+                datetime_from_hour = 8
+                datetime_from_minute = 30
+                date_from = date.today() + timedelta(days=1)
+        elif date.today().weekday() == 4:
+            if datetime.now().hour < 8 & datetime.now().minute < 30:
+                datetime_from_hour = 8
+                datetime_from_minute = 30
+            elif datetime.now().hour < 10:
+                datetime_from_hour = 10
+                datetime_from_minute = 0
+            elif datetime.now().hour < 12 & datetime.now().minute < 30:
+                datetime_from_hour = 12
+                datetime_from_minute = 30
+            elif datetime.now().hour < 14:
+                datetime_from_hour = 14
+                datetime_from_minute = 0
+            elif datetime.now().hour < 15 & datetime.now().minute < 30:
+                datetime_from_hour = 15
+                datetime_from_minute = 30
+            else:
+                date_from = date.today() + timedelta(days=3)
+                datetime_from_hour = 8
+                datetime_from_minute = 30
+        elif date.today().weekday() == 5:
+            date_from = date.today() + timedelta(days=2)
+            datetime_from_hour = 8
+            datetime_from_minute = 30
+        else:
+            date_from = date.today() + timedelta(days=1)
+            datetime_from_hour = 8
+            datetime_from_minute = 30
+
+        date_to = date_from + timedelta(days=7)
+        date_slot = date_from
+
+        time_from = time(datetime_from_hour, datetime_from_minute, 0, 0)
+        time_slot = time_from
+
+        available_datetimes = []
+
+        while date_from <= date_slot <= date_to:
+            if date_slot.weekday() != [5, 6]:
+                    while time_from <= time_slot < time(11, 30, 0, 0) or time(12, 30, 0, 0)<=time_slot<= time(17, 0, 0, 0):
+                        pre_available_time = datetime(date_slot.year, date_slot.month, date_slot.day, time_slot.hour,
+                                                  time_slot.minute, 0)
+                        if not Appointment.objects.filter(appointment_datetime__contains=pre_available_time):
+                            available_datetimes.append(pre_available_time)
+                            time_slot = time_slot + timedelta(minutes=90)
+                        if time_slot==time(11, 30, 0, 0):
+                            time_slot=time_slot + timedelta(minutes=60)
+            date_slot = date_slot + timedelta(days=1)
+
+        return render(request, 'appointment_add.html', {'user': user, 'dogs': dogs, 'available_datetimes':available_datetimes,'firstname': firstname})
     else:
         error = 'You have to sign in first.'
         return render(request, 'registration/login.html', {'error': error})
